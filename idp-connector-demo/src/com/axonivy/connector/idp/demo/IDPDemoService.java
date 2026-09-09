eb7968f34a9619de1cdd2d0ba0520ef8e04d97e6
anh.phamtu <anh.phamtu@axonactive.com>
2026-09-09 11:30:12 +0700

    restclient & demo : adapt new api

--- idp-connector-demo/src/com/axonivy/connector/idp/demo/IDPDemoService.java
+++ idp-connector-demo/src/com/axonivy/connector/idp/demo/IDPDemoService.java
@@ -5,27 +5,22 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
 
-import jakarta.ws.rs.core.Response;
-
 import org.apache.commons.io.IOUtils;
-import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.Strings;
 
 import com.axonivy.connector.idp.demo.dto.SubDocument;
 
-import ch.ivyteam.ivy.application.IApplication;
-import ch.ivyteam.ivy.cm.ContentManagementSystem;
 import ch.ivyteam.ivy.cm.ContentObjectValue;
-import ch.ivyteam.ivy.cm.exec.ContentManagement;
 import ch.ivyteam.ivy.environment.Ivy;
 import ch.ivyteam.ivy.scripting.objects.Binary;
 import ch.ivyteam.ivy.scripting.objects.File;
-import ch.ivyteam.ivy.security.exec.Sudo;
+import jakarta.ws.rs.core.Response;
 
 public class IDPDemoService {
-	private static final String REST_CLIENT_NAME = "IDP-Document-Capturing-API";
+	private static final String REST_CLIENT_KEY = "IDP";
 
 	public static java.io.File exportFromCMS(String cmsUri, String ext) throws IOException {
-		String file = StringUtils.removeStart(cmsUri, "/") + "." + ext;
+		String file = Strings.CS.removeStart(cmsUri, "/") + "." + ext;
 		java.io.File tempFile = new File(file, true).getJavaFile();
 		tempFile.getParentFile().mkdirs();
 		ContentObjectValue cov = Ivy.cms().root().child().file(cmsUri, ext).value().get();
@@ -37,7 +32,7 @@ public class IDPDemoService {
 	}
 	
 	public static File exportIvyFileFromCMS(String cmsUri, String ext) throws IOException {
-		String file = StringUtils.removeStart(cmsUri, "/") + "." + ext;
+		String file = Strings.CS.removeStart(cmsUri, "/") + "." + ext;
 		File tempFile = new File(file, true);
 		tempFile.createNewFile();
 		ContentObjectValue cov = Ivy.cms().root().child().file(cmsUri, ext).value().get();
@@ -57,8 +52,9 @@ public class IDPDemoService {
 		}
 	}
 	
+	/** Writes the page image of a processing result to the CMS file <cmsPath>.jpeg. */
 	public static void getPageImage(String processingId, Integer pageNum, String cmsPath) {
-		Response response = Ivy.rest().client(REST_CLIENT_NAME)
+		Response response = Ivy.rest().client(REST_CLIENT_KEY)
 				.path("/processing/results/{processing_id}/page-images/{page_num}")
 				.resolveTemplate("processing_id", processingId).resolveTemplate("page_num", pageNum)
 				.request().header("Authorization", "ApiKey " + Ivy.var().get("idpConnector.apiKeySecret"))
@@ -68,18 +64,10 @@ public class IDPDemoService {
 			return;
 		}
 		try (InputStream inputStream = response.readEntity(InputStream.class)) {
-			var app = IApplication.current();
-			ContentManagementSystem cms = ContentManagement.cms(app);
-			cms.root().child().file(cmsPath, "jpeg").value().get("").write().inputStream(inputStream);
+			Ivy.cms().root().child().file(cmsPath, "jpeg").value().get("").write().inputStream(inputStream);
 			Ivy.log().debug("cms:{0}", cmsPath);
 		} catch (Exception e) {
 			Ivy.log().error("Error when create PageImage.", e);
 		}
-	}
-	
-	public static void setDemoTaskName(String taskName) {
-		Sudo.run(() ->{
-			Ivy.wfTask().setName(taskName);
-		});
 	}
 }
